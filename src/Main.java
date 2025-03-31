8d1689afd4d93eda196c69277972b435015b9986
김현정 <ajdrns8694@gmail.com>
2025-03-31 15:31:24 +0900

    3commit

--- src/Main.java
+++ src/Main.java
@@ -3,5 +3,6 @@ public class Main {
         System.out.println("안녕하세요");
         System.out.println("안녕하세요2");
         System.out.println("브랜치 만들거야");
+        System.out.println("메인에서 다시 커밋");
     }
 }
