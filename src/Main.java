0f387360e177488af82ba3dec6afe915426fcd5b
김현정 <ajdrns8694@gmail.com>
2025-03-31 15:24:59 +0900

    create branch file

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,7 @@
 public class Main {
     public static void main(String[] args) {
         System.out.println("안녕하세요");
+        System.out.println("안녕하세요2");
+        System.out.println("브랜치 만들거야");
     }
 }
